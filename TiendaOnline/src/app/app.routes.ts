3b7643fbe32261a9007e90018edd2cf37c598b96
[EDU] Jorge Sánchez Marín <jsm000.edu@innovasur.com>
2026-02-16 13:12:44 +0100

    Finalizado TiendaOnline

--- TiendaOnline/src/app/app.routes.ts
+++ TiendaOnline/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { Formulario } from './formulario/formulario';
 import { Error } from './error/error'
 import { ListadoProductos } from './listado-productos/listado-productos';
 import { Login } from './login/login';
+import { LoginGuardian } from './login-guardian';
 
 export const routes: Routes = [
-    {path: '', component: ListadoProductos},
-    {path: 'listado', component: ListadoProductos},
-    {path: 'agregar', component: Formulario},
-    {path: 'editar/:llave', component: Formulario},
+    {path: '', component: ListadoProductos, canActivate: [LoginGuardian]},
+    {path: 'listado', component: ListadoProductos, canActivate: [LoginGuardian]},
+    {path: 'agregar', component: Formulario, canActivate: [LoginGuardian]},
+    {path: 'editar/:llave', component: Formulario, canActivate: [LoginGuardian]},
     {path: 'login', component: Login},
     // Ruta comodin para las no registradas
     {path: '**', component: Error}
